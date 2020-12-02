e64facf829dc2ff493876af52ad7a8ef1bddd0d4
pooja hiremath <54283200+Poojahiremath983@users.noreply.github.com>
2020-12-02 20:50:12 +0530

    Add files via upload

---
+++ pgm6.php
@@ -0,0 +1,16 @@
+<html> 
+<head> 
+<title>Web Page Counter</title> 
+</head> 
+<body bgcolor="pink" text="blue"> 
+<?php 
+$my_file = "count.txt"; 
+$handle = fopen($my_file, "r"); 
+$count = fgets($handle); 
+$count=$count+1; 
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file); 
+fwrite($handle, $count); 
+echo "This page has been viewed $count times"; 
+?> 
+</body> 
+</html
---
+++ pgm7.php
@@ -0,0 +1,18 @@
+<?php 
+date_default_timezone_set('Asia/Kolkata')
+?> 
+<!DOCTYPE html>
+<html>
+   <head>
+        <title></title>
+        <meta http_equiv="refresh" content="1">
+    </head>
+    <body>
+        <p>
+            <?php
+            echo date("h: i : s A");
+            ?>
+            </p>
+    </body>
+    </html>
+
